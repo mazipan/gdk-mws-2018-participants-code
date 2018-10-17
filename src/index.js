9f63914fe701a564dcfb4aa2d7868a74c6bd5f76
Muslim Al Fatih <muslimalfatih@users.noreply.github.com>
2018-10-18 04:36:39 +0700

    Add participants page (#27)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ fetch(ROOT + 'participants.json')
   .then(function(response) {
     return response.json();
   })
-  .then(function(json) {
-    console.log(json);
+  .then(function(participants) {
+    const tableBody = document.getElementsByClassName('table-body')[0]
+
+    participants.forEach((participant, index) => {
+      const tableRow = document.createElement('tr')
+      const { fasilitator, name, email, firebase_link } = participant
+
+      tableRow.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${fasilitator}</td>
+        <td>${name}</td>
+        <td>${email}</td>
+        <td><a href="${firebase_link}">${firebase_link}</a></td>
+      `
+
+      tableBody.appendChild(tableRow)
+
+    })
   });
